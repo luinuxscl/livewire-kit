893823ac8f7e86394e32632a36d98e55310043c0
Luis Sepulveda <lsepulveda@outlook.com>
2025-07-01 11:28:19 -0400

    feat: añadir spatie/laravel-markdown y actualizar vista de prompts

--- resources/views/playground/prompts.blade.php
+++ resources/views/playground/prompts.blade.php
@@ -1,7 +1,7 @@
 <x-layouts.likeplatform :title="__('Prompts')" icon="layout-grid">
 
-    {{-- <x-markdown-viewer>
-        #este es un test
-    </x-markdown-viewer> --}}
+    <x-markdown highlight-theme="monokai-sublime">
+        # Prompts
+    </x-markdown>
 
 </x-layouts.likeplatform>
